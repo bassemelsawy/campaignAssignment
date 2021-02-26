1169ac5f3d4715ea79310fa7bbaf8a48af001d38
Bassem Elsawy <bassemelsawy22@gmail.com>
2021-02-26 03:32:11 -0600

    refactor UT test

--- src/test/java/com/campaign/assignment/util/CampaignUtilTest.java
+++ src/test/java/com/campaign/assignment/util/CampaignUtilTest.java
@@ -1,10 +1,10 @@
 package com.campaign.assignment.util;
 
 import com.campaign.assignment.model.Input;
-import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import java.io.IOException;
 import java.util.*;
 
@@ -21,10 +21,27 @@ class CampaignUtilTest {
 
     @Test
     @DisplayName("should get the right campaign name when call process with a line from input.txt")
-    void process() {
+    void shouldReturnCampaignName_WhenCallProcess() {
         Input input = new Input();
         input.setId("1");
-        input.setSegments(List.of("20231","19691", "13659", "7507", "19024", "4328"));
-        assertThat(campaignUtil.process(input)).isEqualTo("serials");
+        input.setSegments(List.of("22207", "28404", "28113", "24604", "15686", "17632", "7975"));
+        assertThat(campaignUtil.process(input)).isEqualTo("selma");
+    }
+
+
+    @Test
+    @DisplayName("test the distrbution ")
+    void shouldSelectTheValidCampaignBasedOnDistribution_WhenCallProcess() {
+        Input input1 = new Input();
+        input1.setId("1");
+        input1.setSegments(List.of("8023"));
+
+        assertThat(campaignUtil.process(input1)).isEqualTo("seaming");
+
+        Input input2 = new Input();
+        input2.setId("2");
+        input2.setSegments(List.of("8023"));
+
+        assertThat(campaignUtil.process(input2)).isEqualTo("genre");
     }
 }
